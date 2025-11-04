00731dc9416336cb0edadb2ad52095848251a659
kengres <kenguruka24@gmail.com>
2025-11-04 06:18:44 +0000

    bb5e2f902838aee2e427300cc9630f23369d50ba

--- bin/pre-run.sh
+++ bin/pre-run.sh
@@ -4,17 +4,10 @@ function print_red(){
 	echo -e "\e[31m$@\e[0m"
 }
 
-function print_green(){
-	echo -e "\e[32m$@\e[0m"
-}
-
 function check(){
-	echo "---- Checking status code: $1 ----"
 	if [[ $1 != 0 ]]
 		then
-		print_red "SUBMISSION FAILED"
+		print_red "Submission failed"
 		exit 1
-	else
-		print_green "SUCCESS"
 	fi
 }
--- bin/test-config.sh
+++ bin/test-config.sh
@@ -2,7 +2,6 @@
 source ./bin/pre-run.sh
 
 npm ci --no-fund --no-audit
-check $?
 
 node ./dist/index.js ../
 check $?
