ad308087edbe994890915e53e75a4f68597e6541
kengres <kenguruka24@gmail.com>
2025-11-17 10:52:58 +0000

    2496d036c994546a66d67b4d86e2fec7a04a779c

--- bin/test-endpoints-12-ft.sh
+++ bin/test-endpoints-12-ft.sh
@@ -3,9 +3,8 @@ source ./bin/pre-run.sh
 
 npm install -g newman --no-fund --no-audit --loglevel=error
 
-COLLECTION_ID="40482199-cc44329c-06b1-43e3-a7d2-d66e1c6bcd06"
 
-POSTMAN_COLLECTION_URL="https://api.postman.com/collections/$COLLECTION_ID?access_key=PMAT-01HP1EQ3Z0WPQZGS6DMPY0ZB7X"
+POSTMAN_COLLECTION_URL="https://api.postman.com/collections/40482199-cc44329c-06b1-43e3-a7d2-d66e1c6bcd06?access_key=PMAT-01KA8PD5WSNB8NQMRSDW3GRXM0"
 
 timeout 60 bash ./bin/newman.sh $POSTMAN_COLLECTION_URL
 
--- bin/test-endpoints-12.sh
+++ bin/test-endpoints-12.sh
@@ -3,9 +3,7 @@ source ./bin/pre-run.sh
 
 npm install -g newman --no-fund --no-audit --loglevel=error
 
-COLLECTION_ID="23570023-9b9c196e-c509-43ca-9ef9-def9bfc1377d"
-
-POSTMAN_COLLECTION_URL="https://api.postman.com/collections/$COLLECTION_ID?access_key=PMAT-01HP1EQ3Z0WPQZGS6DMPY0ZB7X"
+POSTMAN_COLLECTION_URL="https://api.postman.com/collections/23570023-9b9c196e-c509-43ca-9ef9-def9bfc1377d?access_key=PMAT-01HP1EQ3Z0WPQZGS6DMPY0ZB7X"
 
 timeout 60 bash ./bin/newman.sh $POSTMAN_COLLECTION_URL
 
